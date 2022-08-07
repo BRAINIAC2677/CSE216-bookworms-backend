ed0fb9ea1d524be06ff66c28f42df46803ac70bf
BRAINIAC2677 <brainiac2677@gmail.com>
2022-08-08 01:12:20 +0600

    fix2

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
 
     # local apps
     'rest_framework',
@@ -102,7 +102,6 @@ DATABASES = {
     }
 }
 
-WHITENOISE_USE_FINDERS = True
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
@@ -141,7 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
