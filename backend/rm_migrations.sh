d005f3b30c1bc69bfa4841cac3e906f948467e9d
BRAINIAC2677 <brainiac2677@gmail.com>
2022-08-07 16:39:44 +0600

    changes for deployment in python anywhere

--- backend/rm_migrations.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# run this from the root of the django project where the manage.py located
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path '*/migrations/*.pyc' -delete
