16ae386db8e36d36e37aea7fca333f5cf9a5254d
BRAINIAC2677 <brainiac2677@gmail.com>
2022-08-07 21:31:24 +0600

    changes for heroku deployment again

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +30,7 @@ urlpatterns = [
     path('api/librarystock/', include('librarystock.urls')),
     path('api/bookborrow/', include('bookborrow.urls')),
     path('api/friend/', include('friend.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
 ]
